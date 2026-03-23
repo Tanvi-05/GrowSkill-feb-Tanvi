beda1ac95de0a3689d2d99db4c3921045cbcb3d3
Tanvi-05 <163890187+Tanvi-05@users.noreply.github.com>
2026-03-23 20:33:44 +0530

    DailyAssignment_19_03

---
+++ StudentManagement.java
@@ -0,0 +1,33 @@
+package assignment1_19_03;
+
+public class StudentManagement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int Age=21;
+		int Marks=450;
+		long city_Population=2500000;
+		int Battery=85;
+		char Grade='A';
+		int Bank_Balance=50000;
+		boolean Login_Status=true;
+		long Distance=1200;
+		long Large_Number=900000000;
+		
+		System.out.println("Age : "+Age);
+		System.out.println("Marks : "+Marks);
+		System.out.println("Battery : "+Battery+"%");
+		System.out.println("City Population : "+city_Population);
+		System.out.println("Grade : "+Grade);
+		System.out.println("Bank Balance : "+Bank_Balance);
+		System.out.println("Login Status : "+Login_Status);
+		System.out.println("Distance : "+Distance+ " meters");
+		System.out.println("Large Number : "+Large_Number);
+		
+		
+		
+
+	}
+
+}
