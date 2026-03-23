de4ca022d11cffc89c01b6a4e7ede0606ece4f81
Tanvi-05 <163890187+Tanvi-05@users.noreply.github.com>
2026-03-23 20:35:19 +0530

    DailyAssignment_23_03

---
+++ Program_Variable.java
@@ -0,0 +1,12 @@
+package assignment_23_03;
+
+public class Program_Variable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=5;
+		System.out.println("Value of \"a\":"+a +"\nafter increment:\t"+(++a+1) );
+
+	}
+
+}
