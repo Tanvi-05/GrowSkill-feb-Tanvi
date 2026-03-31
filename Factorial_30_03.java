c3297a294247af1602682b3c0e210170522a2f33
Tanvi-05 <163890187+Tanvi-05@users.noreply.github.com>
2026-03-31 23:53:51 +0530

    dailyAssignment

---
+++ Factorial_30_03.java
@@ -0,0 +1,19 @@
+package dailyAssignment;
+
+public class Factorial_30_03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		        int number = 5; // Input
+		        long factorial = 1;
+
+		        for (int i = 1; i <= number; i++) {
+		            factorial *= i;
+		        }
+
+		        System.out.println(number + "! = " + factorial);
+		    }
+		
+
+}
